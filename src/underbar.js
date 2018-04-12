af17e8711390597323b1f4ab76d0c751d8628274
Chrystal Zou <chrystalzou@gmail.com>
2018-04-12 10:50:00 -0700

    Modified _.each function to be able to accept indices and collections

--- src/underbar.js
+++ src/underbar.js
@@ -55,7 +55,7 @@
   _.each = function(collection, iterator) {
     if (Array.isArray(collection)) {
       for (var i = 0; i < collection.length; i++) {
-        iterator(collection[i]);
+        iterator(collection[i], i, collection);
       }
     } else {
       for (var key in collection) {
