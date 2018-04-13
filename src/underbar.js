f88867b0295a14b5cca2aff725b52d939836948d
Chrystal Zou <chrystalzou@gmail.com>
2018-04-12 20:44:11 -0700

    Wrote _.delay function

--- src/underbar.js
+++ src/underbar.js
@@ -319,6 +319,15 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var result;
+    var args = Array.prototype.slice.call(arguments);
+    var passArgs = args.slice(2, args.length);
+
+    window.setTimeout(function() {
+      result = func.apply(this, passArgs);
+    }, wait);
+
+    return result;
   };
 
 
