59b54b1e43462f32c6c9c059b1894e539d25008e
Chrystal Zou <chrystalzou@gmail.com>
2018-04-12 17:07:03 -0700

    Another modification

--- src/underbar.js
+++ src/underbar.js
@@ -104,13 +104,11 @@
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
     var uniqueArr = [];
-
     _.each(array, function(item){
       if(_.indexOf(uniqueArr, item) === -1){
         uniqueArr.push(item);
       }
     });
-
     return uniqueArr;
   };
 
